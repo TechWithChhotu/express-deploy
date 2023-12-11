8faa78ae213e117a82203e814e67e1ea2a67be83
Chhotu kumar <110243856+TechWithChhotu@users.noreply.github.com>
2023-12-11 07:03:37 +0530

    some change for frontend connecton

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 
 app.use("/", (req, res) => {
   res.json({ message: "Hello from express" });
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST"], // Include POST method
+    credentials: true,
+  })
+);
 
 const PORT = 9000;
 
