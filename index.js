eb2ae1f23d2e5deda7b88cc25aba2aace7fdcc42
Chhotu kumar <110243856+TechWithChhotu@users.noreply.github.com>
2023-12-11 07:06:48 +0530

    Some change in path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use("/", (req, res) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"], // Include POST method
     credentials: true,
   })
