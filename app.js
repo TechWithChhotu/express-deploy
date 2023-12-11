3c9e09284b44b662fe5fd7f4a58a6ca0b6829b0c
Chhotu kumar <110243856+TechWithChhotu@users.noreply.github.com>
2023-12-11 09:44:38 +0530

    Cors

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(
   })
 );
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// Enable CORS for all routes
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/", router);
